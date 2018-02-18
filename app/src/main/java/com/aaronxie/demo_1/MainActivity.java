41b6ac377a64e5bdf46808331d4cb1f61b5b30ee
aaronxie <1045882833@qq.com>
2018-02-18 14:01:16 +0800

    我在提交

--- app/src/main/java/com/aaronxie/demo_1/MainActivity.java
+++ app/src/main/java/com/aaronxie/demo_1/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //我正在测试
         Log.i("TAG","first");
         Log.i("TAG","second");
     }
